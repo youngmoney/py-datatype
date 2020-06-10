da0065bc958375796c04ef56fa7e4093ac1824ad
Taylor L Money <git@taylorlmoney.com>
2020-06-10 09:04:46 -0700

    up version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-datatype",
-    version="0.0.1",
+    version="0.0.2",
     author="Taylor L Money",
     author_email="git@taylorlmoney.com",
     description="",
