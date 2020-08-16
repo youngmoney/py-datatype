8942b370e1ae72c02c61f1bd0e8c912091cdeca9
Taylor L Money <git@taylorlmoney.com>
2020-08-16 11:46:58 -0700

    release version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-datatype",
-    version="0.0.3",
+    version="0.0.4",
     author="Taylor L Money",
     author_email="git@taylorlmoney.com",
     description="",
